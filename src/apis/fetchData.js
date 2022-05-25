003747836a4961e14a9e89f90b06f9bf62012801
khanhandli <y.nguyen@vinaseco.vn>
2022-05-25 20:34:04 +0700

    bug/fix api url

--- src/apis/fetchData.js
+++ src/apis/fetchData.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const URL = 'http://localhost:5000';
+const URL = 'https://cocoapp-server.herokuapp.com';
 axios.defaults.withCredentials = true;
 
 export const getDataAPI = async (url, token) => {
